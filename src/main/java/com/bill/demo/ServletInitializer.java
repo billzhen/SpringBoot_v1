588be7acd891b2b20c3e28c90075f6748b502634
billzhen <billzybw@163.com>
2023-02-12 15:02:40 +0800

    update

--- src/main/java/com/bill/demo/ServletInitializer.java
+++ src/main/java/com/bill/demo/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("ServletInitializer.configure()");
 		return application.sources(DemoApplication.class);
 	}
 
