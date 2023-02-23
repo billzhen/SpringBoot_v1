70f3b787c2472e0f4cfa05f07cd6b7da344472e6
billzhen <billzybw@163.com>
2023-02-23 15:26:57 +0800

    SupportGPT3

--- src/main/java/com/bill/demo/config/ChatGptConfig.java
+++ src/main/java/com/bill/demo/config/ChatGptConfig.java
@@ -4,7 +4,7 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "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";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
--- src/main/java/com/bill/demo/controller/SearchGPT.java
+++ src/main/java/com/bill/demo/controller/SearchGPT.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/search")
 public class SearchGPT{
     private final BotService botService;
     @Autowired
